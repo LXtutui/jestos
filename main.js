992c5b7dcb2d79daed5e77f1afb044b225a5d9f5
LXtutui <113383859+LXtutui@users.noreply.github.com>
2023-06-21 14:34:28 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+var previsao1="";
+Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90
+  });
+
+  var camera = document.getElementById("camera");
+
+Webcam.attach( '#camera' );
+
+function takeSnapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("camera").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+
+console.log('ml5 version:', ml5.version);
+  
+  var classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/ieTY3PA9y/model.json',modelLoaded);
+
+  function modelLoaded(){
+    console.log("modelo carregado");
+  }
+  function speak(){
+    var api_speak=window.speechSynthesis;
+    var dado_fala1="a previsão é " + previsao1;
+    var utter_this=new SpeechSynthesisUtterance(dado_fala1);
+    api_speak.speak(utter_this);
+  }
+  function check(){
+    var mostrar_imagem=document.getElementById("captured_image");
+    classifier.classify(mostrar_imagem, gotResult());
+  }
+  function gotResult(error, results){
+    if (error){
+      console.error(error);
+    }else{
+      console.log(results);
+      document.getElementById("result_emotion_name").innerHTML=dado_fala1[0].label;
+      previsao1=results[0].label;
+      if(previsao1=="vitoria"){
+        document.getElementById("update_emoji").innerHTML="&#9996;";
+      }
+      if(previsao1=="tranquilo"){
+        document.getElementById("update_emoji").innerHTML="&#129305;";
+      }
+      if(previsao1=="legal"){
+        document.getElementById("update_emoji").innerHTML="&#128077;";
+      }
+    }
+    }
